6a61263dc22db5638943bae2ae47e8178b71c38c
gmugwe <ggmugwe@gmail.com>
2017-04-13 20:30:36 +0300

    second commit

--- js/scripts.js
+++ js/scripts.js
@@ -17,11 +17,13 @@
      };
 
   // start of user interface (front-end) logic:
+
   $(document).ready(function() {
-    $("form#add").submit(funtion(event){
-      event.preventDefult();
-      var number1 = parseInt($("#add1").val());
-      var number2 = parseInt($("#add2").val());
-      alert(add(number1, number2));
+      $("form#add").submit(function(event) {
+        event.preventDefault();
+        var number1 = parseInt($("#add1").val());
+        var number2 = parseInt($("#add2").val());
+        var result = add(number1, number2);
+        $("#output").text(result);
+      });
     });
-  });
